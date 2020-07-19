5e8eea2c27ec3afeedd70f5fb0193f15a5db652b
陈金亚 <chenjinya@chenjinyadeMacBook-Pro.local>
2020-07-19 20:18:27 +0800

    feat: null time

--- time.go
+++ time.go
@@ -1,6 +1,8 @@
 package godash
 
 import (
+	"database/sql"
+	"encoding/json"
 	"log"
 	"math/rand"
 	"time"
@@ -12,4 +14,28 @@ func RandSleep() {
 	seed := r.Int() % 1000
 	log.Println("Rand Sleep:", seed)
 	time.Sleep(time.Duration(seed) * time.Millisecond)
+}
+
+
+type NullTime struct {
+	sql.NullTime
+}
+
+func (nt NullTime) MarshalJSON()([]byte, error){
+	if nt.Valid {
+		return json.Marshal(nt.Time)
+	} else {
+		return []byte(""), nil
+	}
+}
+
+func (nt *NullTime) UnmarshalJSON(data []byte) error{
+	if nil != data {
+		nt.Valid = true
+		return json.Unmarshal(data, &nt.Time)
+	} else {
+		nt.Time = time.Time{}
+		nt.Valid = false
+		return nil
+	}
 }
